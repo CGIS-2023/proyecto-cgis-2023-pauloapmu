d55c37e92d0dc90cb998d33e4795b12d70b4151f
pauloapmu <paulopamu@alum.us.es>
2023-03-23 20:52:00 +0100

    entregable2

---
+++ app/Http/Controllers/AlergenoController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AlergenoController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\MedicoController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +11,26 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware(['auth', 'tipo_usuario:3'])->group(function () {
+    Route::get('/nutricionistas/create', [NutricionistaController::class, 'create'])->name('nutricionistas.create');
+    Route::post('/nutricionistas', [NutricionistaController::class, 'store'])->name('nutricionistas.store');
+    Route::delete('/nutricionistas/{nutricionista}', [NutricionistaController::class, 'destroy'])->name('nutricionistas.destroy');
+    
+});
+
+
+require __DIR__.'/auth.php';
